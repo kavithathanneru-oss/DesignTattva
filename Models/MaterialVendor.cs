a31d22f1dccbcce14a99bad6f489e8823c31c098
kavithathanneru-oss <kavitha.thanneru@gmail.com>
2026-03-12 19:01:17 +0530

    Add MaterialVendor model class

---
+++ Models/MaterialVendor.cs
@@ -0,0 +1,9 @@
+public class MaterialVendor
+{
+    public int VendorID { get; set; }
+    public string VendorName { get; set; }
+    public string MaterialCategory { get; set; }
+    public string Contact { get; set; }
+    public string Address { get; set; }
+    public string PaymentTerms { get; set; }
+}
